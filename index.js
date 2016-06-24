6b8eeb948eab30a380371f384b244dab72c7e1cc
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-06-24 00:58:07 -0700

    delete stub

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ HyperString.prototype.insert = function (prev, chr, cb) {
     process.nextTick(function () { cb(null, 'a') })
 }
 
+HyperString.prototype.delete = function (at, cb) {
+    process.nextTick(function () { cb(null) })
+}
+
 // HyperString.prototype.createStringStream = function () {
 // }
 
