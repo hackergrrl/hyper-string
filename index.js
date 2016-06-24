bef00ca3beba97ea40145c1e28226166d9070b1d
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-06-24 11:47:18 -0700

    internal string dag impl

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var hyperlog = require('hyperlog')
+var hindex = require('hyperlog-index')
+var memdb = require('memdb')
 
 module.exports = HyperString
 
@@ -9,6 +11,38 @@ function HyperString (db, opts) {
   opts.valueEncoding = 'json'
 
   this.log = hyperlog(db, opts)
+
+  // Representation of the string's current state, as a DAG.
+  // This makes traversal from the beginning of the document quick!
+  var stringDag = {}
+  this.index = hindex({
+    log: this.log,
+    db: memdb(),  // for now, separate in-memory
+    map: function (row, next) {
+      if (row.value.op === 'insert') {
+        // add character
+        var character = {
+          chr: row.value.chr,
+          links: []
+        }
+        stringDag[row.key] = character
+
+        // add links
+        if (row.value.prev) {
+          var prev = stringDag[row.value.prev]
+          if (!prev) throw new Error('woah, this should never happen!')
+          prev.links.push(row.key)
+        }
+      } else {
+        throw new Error('unsupported operation: ' + row.value.op)
+      }
+      next()
+    }
+  })
+
+  this.index.ready(function () {
+//    console.log('READY', stringDag)
+  })
 }
 
 HyperString.prototype.insert = function (prev, chr, cb) {
