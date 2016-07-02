30c739c6cef8e8d5a158550203bc88b910dd493f
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-07-02 19:01:18 -0400

    test "text" and "chars"

--- test/basic.js
+++ test/basic.js
@@ -32,8 +32,12 @@ test('insertions', function (t) {
   })
 })
 
-test('get full string', function (t) {
-  t.plan(2)
+test('deletions', function (t) {
+})
+
+test('text + chars', function (t) {
+  t.plan(4)
+
   var str = hstring(memdb())
 
   str.insert(null, 'H', function (err, op) {
@@ -47,10 +51,42 @@ test('get full string', function (t) {
     })
   })
 
-  var expected = 'Heyllo'
-
+  // text
   str.text(function (err, text) {
+    var expected = 'Heyllo'
     t.equals(err, null)
     t.equals(text, expected)
+  })
+
+  // chars
+  str.chars(function (err, chars) {
+    var expected = [
+      {
+        chr: 'H',
+        pos: '43ba8b3fec78c2c3da893fc67792bc45f330ae8083b5e54f1fd16ba9df4fa9c4'
+      },
+      {
+        chr: 'e',
+        pos: '136ac665c69975abd46101cea25d76431f7f7ae6378bafd2d2801be05ebaaf94'
+      },
+      {
+        chr: 'y',
+        pos: '78f039133526f543089160ae7145d980b79737d224b61ee98e252fb0cc61ba79'
+      },
+      {
+        chr: 'l',
+        pos: '745e47b23097e54f034c84932a5721bceb16d54e3eb74d15faee110b8b809b1e'
+      },
+      {
+        chr: 'l',
+        pos: '54bfc709d7d3190b8f27a0023da0a87ba1f15950c75a8d59258b955f2261bc54'
+      },
+      {
+        chr: 'o',
+        pos: '16ab3a94f5aee62a3031da58c6b9f46c0172e2a0ddffc90b6ebe789c1a879e2b'
+      }
+    ]
+    t.equals(err, null)
+    t.deepEquals(chars, expected)
   })
 })
