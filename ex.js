2b265861d84a8a15f049f8b8c456af395d1aa4b9
Stephen Whitmore <sww@eight45.net>
2017-11-09 21:12:31 -0800

    chore: example

--- ex.js
+++ ex.js
@@ -3,13 +3,12 @@ var memdb = require('memdb')
 
 var str = hstring(memdb())
 
-str.insert(null, null, 'Hola', function (err, ops) {
+str.insert(null, null, 'Hola', function (_, ops) {
   // ops is an array of four unique IDs
   str.insert(null, ops[0], '¡')
-  str.insert(ops[ops.length-1], null, '!')
+  str.insert(ops[ops.length - 1], null, '!')
 })
 
-str.text(function (err, text) {
+str.text(function (_, text) {
   console.log(text)
 })
-
