de12a578e533f1a926cd51ef4c2c501776c3f799
Stephen Whitmore <sww@eight45.net>
2018-02-01 20:21:16 -0800

    refactor: factor out string logic to fork-string

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var assert = require('assert')
 var hyperlog = require('hyperlog')
 var memdb = require('memdb')
-var debug = require('debug')('hyper-string')
 var SafeIndex = require('./safe-index')
+var fstring = require('fork-string')
 
 function noop () {}
 
@@ -15,17 +15,14 @@ function HyperString (db, opts) {
   opts.valueEncoding = 'json'
 
   this.log = hyperlog(db, opts)
+  this.data = fstring()
 
-  // Representation of the string's current state, as a DAG.
-  // This makes traversal from the beginning of the document quick!
+  var self = this
   this.index = new SafeIndex({
     log: this.log,
     db: memdb(),  // for now
     init: function () {
-      return {
-        nodes: {},
-        roots: []
-      }
+      return { data: self.data }
     },
     map: indexMapFn
   })
@@ -33,111 +30,15 @@ function HyperString (db, opts) {
 
 function indexMapFn (index, row, next) {
   if (row.value.op === 'insert') {
-    insertRow()
-    debug('INDEX stringDag[' + row.key + '] = ' + row.value.chr)
+    index.data.insert(row.value.prev, row.value.next, row.value.txt, row.key)
   } else if (row.value.op === 'delete') {
-    deleteRow()
+    index.data.delete(row.value.from, row.value.to, row.key)
   } else {
     // silently ignore other node types
     // return next(new Error('unsupported operation:', row.value.op))
   }
 
-  function link (fromKey, toKey) {
-    var from = index.nodes[fromKey]
-    var to = index.nodes[toKey]
-
-    if (!from || !to) return
-
-    from.outgoingLinks.push(toKey)
-    to.incomingLinks.push(fromKey)
-  }
-
   next()
-
-  function insertRow () {
-    var chars = []
-
-    // create individual character entries & link them into the dag
-    for (var i = 0; i < row.value.txt.length; i++) {
-      var key = row.key + '@' + i
-      index.nodes[key] = {
-        chr: row.value.txt[i],
-        outgoingLinks: [],
-        incomingLinks: []
-      }
-      chars.push(key)
-
-      // link 1st character and 'prev'
-      if (i === 0) {
-        link(row.value.prev, key)
-      }
-
-      // link this character to the previous one
-      if (i > 0) {
-        link(chars[i - 1], key)
-      }
-
-      // link last character to 'next'
-      if (i === row.value.txt.length - 1 && row.value.next) {
-        link(key, row.value.next)
-      }
-    }
-
-    // If no prev, make it a root
-    // Case 1: no prev + no next => new root
-    if (!row.value.prev && !row.value.next) {
-      index.roots.push(chars[0])
-      index.roots.sort()
-    }
-
-    // Case 2: no prev + valid next => new root (replace old root)
-    if (!row.value.prev && row.value.next) {
-      // Find + cull the old root
-      for (i = 0; i < index.roots.length; i++) {
-        var root = index.roots[i]
-        if (row.value.next === root) {
-          index.roots.splice(i, 1)
-        }
-      }
-      // Add the new root
-      index.roots.push(chars[0])
-      index.roots.sort()
-    }
-  }
-
-  // mark all affected characters as deleted
-  function deleteRow () {
-    var visited = {}
-    var stack = [row.value.from]
-
-    while (stack.length > 0) {
-      var key = stack.pop()
-
-      if (visited[key]) {
-        continue
-      }
-
-      var dagnode = index.nodes[key]
-
-      // bail if there are other nodes to visit before this one
-      var needToBail = false
-      for (var i = 0; i < dagnode.incomingLinks.length; i++) {
-        if (!visited[dagnode.incomingLinks[i]] && key !== row.value.from) {
-          needToBail = true
-        }
-      }
-      if (needToBail) continue
-
-      index.nodes[key].deleted = true
-      visited[key] = true
-
-      if (key === row.value.to) {
-        break
-      }
-
-      stack.push.apply(stack, dagnode.outgoingLinks)
-    }
-  }
 }
 
 HyperString.prototype.insert = function (prev, next, string, done) {
@@ -185,58 +86,17 @@ HyperString.prototype.delete = function (from, to, done) {
 }
 
 HyperString.prototype.chars = function (cb) {
-  this.index.ready(function (index) {
-    var string = indexToCharData(index)
-    if (cb) cb(null, string)
+  var self = this
+  this.index.ready(function () {
+    cb(null, self.data.chars())
   })
-
-  function indexToCharData (index) {
-    var string = []
-    var visited = {}
-    var stack = index.roots.slice()
-
-    while (stack.length > 0) {
-      var key = stack.pop()
-
-      if (visited[key]) {
-        continue
-      }
-
-      var dagnode = index.nodes[key]
-
-      // bail if there are other nodes to visit before this one
-      var needToBail = false
-      for (var i = 0; i < dagnode.incomingLinks.length; i++) {
-        if (!visited[dagnode.incomingLinks[i]]) {
-          needToBail = true
-        }
-      }
-      if (needToBail) continue
-
-      if (!dagnode.deleted) {
-        var elem = {
-          chr: dagnode.chr,
-          pos: key
-        }
-        string.push(elem)
-      }
-      visited[key] = true
-      stack.push.apply(stack, dagnode.outgoingLinks)
-    }
-
-    return string
-  }
 }
 
 // The current state of the hyperstring, serialized to a plain string.
 HyperString.prototype.text = function (cb) {
-  this.chars(function (err, chars) {
-    if (err) return cb(err)
-
-    var text = chars.map(function (c) { return c.chr })
-    text = text.join('')
-
-    cb(null, text)
+  var self = this
+  this.index.ready(function () {
+    cb(null, self.data.text())
   })
 }
 
